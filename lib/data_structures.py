3a7abf0a969a91ed79a58ef8c9d71b73ae9d0681
amanda ranero <amandaranero@gmail.com>
2023-04-04 00:01:56 -0400

    finished, look over for simpler code

--- lib/data_structures.py
+++ lib/data_structures.py
@@ -17,22 +17,59 @@ spicy_foods = [
 ]
 
 def get_names(spicy_foods):
-    pass
+    get_names = []
+    for i in range(len(spicy_foods)):
+        get_names.append(spicy_foods[i]["name"])
+    return get_names
+  
 
 def get_spiciest_foods(spicy_foods):
-    pass
+    get_spiciest_foods = []
+    for i in range(len(spicy_foods)):
+       if spicy_foods[i]["heat_level"] > 5:
+        get_spiciest_foods.append(spicy_foods[i])
+    return get_spiciest_foods
+      
+    
 
 def print_spicy_foods(spicy_foods):
-    pass
+    for i in range(len(spicy_foods)):
+        name = spicy_foods[i]["name"]
+        cuisine = spicy_foods[i]["cuisine"]
+        heat_level =  (spicy_foods[i]["heat_level"])
+        heat_emoji = "🌶" * (heat_level)
+        print(f"{name} ({cuisine}) | Heat Level: {heat_emoji}")
+        
+        
+
 
 def get_spicy_food_by_cuisine(spicy_foods, cuisine):
-    pass
+    for i in range(len(spicy_foods)):
+        if cuisine == spicy_foods[i]["cuisine"]:
+            return spicy_foods[i]
 
 def print_spiciest_foods(spicy_foods):
-    pass
+    for i in range(len(spicy_foods)):
+        name = spicy_foods[i]["name"]
+        cuisine = spicy_foods[i]["cuisine"]
+        heat_level =  (spicy_foods[i]["heat_level"])
+        heat_emoji = "🌶" * (heat_level)
+        if heat_level > 5:
+            print(f"{name} ({cuisine}) | Heat Level: {heat_emoji}")
 
 def get_average_heat_level(spicy_foods):
-    pass
+    heat_level_array = []
+    for i in range(len(spicy_foods)):
+        heat_level_array.append(spicy_foods[i]["heat_level"])
+    average = sum(heat_level_array)/len(heat_level_array)
+    return average
+        
 
 def create_spicy_food(spicy_foods, spicy_food):
-    pass
+    new_food = []
+    for i in range (len(spicy_foods)):
+        new_food.append(spicy_foods[i])
+    new_food.append(spicy_food)
+    return new_food
+
+  
